c9b042096f247a589b5640d55290fa523ed11ec0
Edward24R <eduardo.rivera24@upr.edu>
2023-03-08 21:42:36 -0400

    Testing commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-
+//testing for github
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <img src="/images/logo-instinct.png" alt="My Logo" className='logo' style={{ position: 'absolute', top: 10, left: 1780 }}/>
+      <img src="/images/logo-instinct.png" alt="My Logo" className='logo' style={{ position: 'absolute', top: 20, left: 1780 }}/>
        
-
- 
       </header>
     </div>
   );
