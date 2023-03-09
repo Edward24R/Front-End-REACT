0666c8b963eaedc3d87c01cf08aa1a90693a4472
Edward24R <eduardo.rivera24@upr.edu>
2023-03-08 21:11:37 -0400

    First Commit and Push

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src="/images/logo-instinct.png" alt="My Logo" className='logo' style={{ position: 'absolute', top: 10, left: 1780 }}/>
+       
+
+ 
       </header>
     </div>
   );
